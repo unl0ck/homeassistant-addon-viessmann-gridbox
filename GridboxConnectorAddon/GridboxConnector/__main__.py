3bf4a25ffb053f67a69e7e1723bcfbe53ea6c3e1
Simon Helming <simon@helming.xyz>
2023-07-25 13:08:52 +0200

    Initial Commit

---
+++ GridboxConnectorAddon/GridboxConnector/__main__.py
@@ -0,0 +1,12 @@
+import os
+import json
+from GridboxConnector import GridboxConnector
+
+if __name__ == '__main__':
+    print(os.environ)
+    f = open('/data/config.json')
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(f)
+    f.close()
+    GridboxConnector(data).retrieve_live_data()
---
+++ GridboxConnectorAddon/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bashio
+
+echo "Hello world!"
+ls /data
+cd /build/
+python3 GridboxConnector
