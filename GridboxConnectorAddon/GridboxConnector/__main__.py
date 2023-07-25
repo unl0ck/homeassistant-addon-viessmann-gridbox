ab0dedffd20e10d46c8547cc8dd79f199dd6ba04
Simon Helming <simon@helming.xyz>
2023-07-25 13:29:17 +0200

    renamed config.json file

--- GridboxConnectorAddon/GridboxConnector/__main__.py
+++ GridboxConnectorAddon/GridboxConnector/__main__.py
@@ -4,7 +4,7 @@ from GridboxConnector import GridboxConnector
 
 if __name__ == '__main__':
     print(os.environ)
-    f = open('/data/config.json')
+    f = open('/data/cloudSettings.json')
     # returns JSON object as 
     # a dictionary
     data = json.load(f)
