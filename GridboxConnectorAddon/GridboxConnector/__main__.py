4eafb4d349874ae708ec7349c5d89506dccd7176
Simon Helming <simon@helming.xyz>
2023-07-27 06:21:57 +0200

    added mqtt login data

--- GridboxConnectorAddon/GridboxConnector/__main__.py
+++ GridboxConnectorAddon/GridboxConnector/__main__.py
@@ -11,26 +11,30 @@ if __name__ == '__main__':
     # returns JSON object as 
     # a dictionary
     data = json.load(f)
+    f.close()
     USER = os.getenv('USERNAME')
     PASSWORD = os.environ.get('PASSWORD')
-    f.close()
+    mqtt_user = os.getenv('MqttUser')
+    mqtt_pw = os.getenv('MqttPw')
+    mqtt_server = os.getenv('MqttServer')
+    mqtt_port = os.getenv('MqttPort')
     data["login"]["username"] = USER
     data["login"]["password"] = PASSWORD
     # Configure the required parameters for the MQTT broker
-    mqtt_settings = Settings.MQTT(host="localhost")
+    mqtt_settings = Settings.MQTT(host=mqtt_server, username=mqtt_user, password=mqtt_pw)
 
     # Define the device. At least one of `identifiers` or `connections` must be supplied
-    device_info = DeviceInfo(name="Photovoltaic", identifiers="viessmann_gridbox")
+    device_info = DeviceInfo(name="Viessmann Gridbox", identifiers="viessmann_gridbox")
 
     # Associate the sensor with the device via the `device` parameter
     # `unique_id` must also be set, otherwise Home Assistant will not display the device in the UI
-    production_sensor_info = SensorInfo(name="Production", device_class="energy", unique_id="gridbox_production", device=device_info, unit_of_measurement="w")
+    production_sensor_info = SensorInfo(name="Production", device_class="power", unique_id="gridbox_production", device=device_info, unit_of_measurement="W")
     production_settings = Settings(mqtt=mqtt_settings, entity=production_sensor_info)
 
-    grid_sensor_info = SensorInfo(name="Grid", device_class="energy", unique_id="gridbox_grid", device=device_info, unit_of_measurement="w")
+    grid_sensor_info = SensorInfo(name="Grid", device_class="power", unique_id="gridbox_grid", device=device_info, unit_of_measurement="W")
     grid_settings = Settings(mqtt=mqtt_settings, entity=grid_sensor_info)
 
-    photovoltaic_sensor_info = SensorInfo(name="Photovoltaic", device_class="energy", unique_id="gridbox_photovoltaic", device=device_info, unit_of_measurement="w")
+    photovoltaic_sensor_info = SensorInfo(name="Photovoltaic", device_class="power", unique_id="gridbox_photovoltaic", device=device_info, unit_of_measurement="W")
     photovoltaic_settings = Settings(mqtt=mqtt_settings, entity=photovoltaic_sensor_info)
     
 
--- GridboxConnectorAddon/run.sh
+++ GridboxConnectorAddon/run.sh
@@ -3,6 +3,10 @@
 
 export USERNAME=$(bashio::config 'username')
 export PASSWORD=$(bashio::config 'password')
+export MqttUser=$(bashio::services "mqtt" "username")
+export MqttPw=$(bashio::services "mqtt" "password")
+export MqttServer=$(bashio::services "mqtt" "host")
+export MqttPort=$(bashio::services "mqtt" "port")
 echo "Hello world!"
 ls -lash /data
 cat /data/options.json
