2657b3ecd125853917ce70778d6d26af0d43edbe
unl0ck <sa_helming@icloud.com>
2024-04-17 16:00:52 +0200

    Update run.sh

--- GridboxConnectorAddon/run.sh
+++ GridboxConnectorAddon/run.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
-
+export MqttUser=$(bashio::config 'OverrideMqttUser')
+export MqttPw=$(bashio::config 'OverrideMqttPw')
+export MqttServer=$(bashio::config 'OverrideMqttServer')
+export FiatChamp_MqttPort=$(bashio::config 'OverrideMqttPort')
+  
+test "$MqttUser" = "null" && export MqttUser=$(bashio::services "mqtt" "username")
+test "$MqttPw" = "null" && export MqttPw=$(bashio::services "mqtt" "password")
+test "$MqttServer" = "null" && export MqttServer=$(bashio::services "mqtt" "host")
+test "$MqttPort" = "null" && export MqttPort=$(bashio::services "mqtt" "port")
 export USERNAME=$(bashio::config 'username')
 export PASSWORD=$(bashio::config 'password')
-export MqttUser=$(bashio::services "mqtt" "username")
-export MqttPw=$(bashio::services "mqtt" "password")
-export MqttServer=$(bashio::services "mqtt" "host")
-export MqttPort=$(bashio::services "mqtt" "port")
-echo "Hello world!"
 ls -lash /data
 cat /data/options.json
 cd /build/
