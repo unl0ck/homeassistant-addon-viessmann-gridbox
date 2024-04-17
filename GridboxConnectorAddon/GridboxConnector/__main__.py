7c3c170c6354570610a972cd63bde1c5e94c025d
unl0ck <sa_helming@icloud.com>
2024-04-17 07:30:21 +0200

    Update __main__.py

--- GridboxConnectorAddon/GridboxConnector/__main__.py
+++ GridboxConnectorAddon/GridboxConnector/__main__.py
@@ -1,9 +1,9 @@
 import os
 import json
-from GridboxConnector import GridboxConnector
 import time
 from ha_mqtt_discoverable import Settings, DeviceInfo
 from ha_mqtt_discoverable.sensors import Sensor, SensorInfo
+from gridbox_connector import GridboxConnector
 
 if __name__ == '__main__':
     f = open('/build/cloudSettings.json')
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     data = json.load(f)
     f.close()
     print("Start Viessmann Connector")
-    #print("====Version {}====".format(data["version"])
+    print(f"====Version {data["version"]}====")
     
     options_file = open('/data/options.json')
     options_json = json.load(options_file)
@@ -27,14 +27,11 @@ if __name__ == '__main__':
     data["login"]["password"] = PASSWORD
     print(data["login"])
     one_time_print = True
-    # Configure the required parameters for the MQTT broker
+
     mqtt_settings = Settings.MQTT(host=mqtt_server, username=mqtt_user, password=mqtt_pw)
 
-    # Define the device. At least one of `identifiers` or `connections` must be supplied
     device_info = DeviceInfo(name="Viessmann Gridbox", identifiers="viessmann_gridbox", manufacturer="Viessmann", model="Vitocharge 2.0")
 
-    # Associate the sensor with the device via the `device` parameter
-    # `unique_id` must also be set, otherwise Home Assistant will not display the device in the UI
     production_sensor_info = SensorInfo(name="Production", device_class="power", unique_id="gridbox_production", device=device_info, unit_of_measurement="W")
     production_settings = Settings(mqtt=mqtt_settings, entity=production_sensor_info)
 
@@ -44,8 +41,15 @@ if __name__ == '__main__':
     photovoltaic_sensor_info = SensorInfo(name="Photovoltaic", device_class="power", unique_id="gridbox_photovoltaic", device=device_info, unit_of_measurement="W")
     photovoltaic_settings = Settings(mqtt=mqtt_settings, entity=photovoltaic_sensor_info)
 
-    #Battery Section
-    
+    # consumption
+    direct_consumption_household_sensor_info = SensorInfo(name="DirectConsumptionHousehold", device_class="power", unique_id="gridbox_direct_consumption_household", device=device_info, unit_of_measurement="W")
+    direct_consumption_household_settings = Settings(mqtt=mqtt_settings, entity=direct_consumption_household_sensor_info)
+
+    direct_consumption_heatpump_sensor_info = SensorInfo(name="DirectConsumptionHeatPump", device_class="power", unique_id="gridbox_direct_consumption_heatpump", device=device_info, unit_of_measurement="W")
+    direct_consumption_heatpump_settings = Settings(mqtt=mqtt_settings, entity=direct_consumption_heatpump_sensor_info)
+
+
+    # Battery Section
     battery_sensor_info_sum = SensorInfo(name="Battery Sum Level", device_class="battery", unique_id="gridbox_battery_sum", device=device_info, unit_of_measurement="%")
     battery_settings_sum = Settings(mqtt=mqtt_settings, entity=battery_sensor_info_sum)
 
@@ -60,10 +64,14 @@ if __name__ == '__main__':
     grid_sensor = Sensor(grid_settings)
     photovoltaic_sensor = Sensor(photovoltaic_settings)
 
-    #Battery Section
+    # Battery
     battery_sum_level = Sensor(battery_settings_sum)
     battery_sum_capacity = Sensor(battery_settings_capacity_sum)
     battery_sum_power = Sensor(battery_settings_power_sum)
+
+    # Consumption
+    direct_consumption_household_sensor = Sensor(direct_consumption_household_settings)
+    direct_consumtion_heatpump_sensor = Sensor(direct_consumption_heatpump_settings)
 
     gridboxConnector = GridboxConnector(data)
     while True:
@@ -75,13 +83,14 @@ if __name__ == '__main__':
             production_sensor.set_state(measurement["production"])
             grid_sensor.set_state(measurement["grid"])
             photovoltaic_sensor.set_state(measurement["photovoltaic"])
-            if "battery" in measurement:
-                battery_sum_level.set_state(float(measurement["battery"]["stateOfCharge"])*100)
-                battery_sum_capacity.set_state(float(measurement["battery"]["capacity"]))
-                battery_sum_power.set_state(float(measurement["battery"]["power"]))
-        else:
-            print("measurement does not have values {}".format(measurement))
-            gridboxConnector = GridboxConnector(data)
+        if "battery" in measurement:
+            battery_sum_level.set_state(float(measurement["battery"]["stateOfCharge"])*100)
+            battery_sum_capacity.set_state(float(measurement["battery"]["capacity"]))
+            battery_sum_power.set_state(float(measurement["battery"]["power"]))
+        if "directConsumptionHousehold" in measurement:
+            direct_consumption_household_sensor.set_state(float(measurement["directConsumptionHousehold"]))
+        if "directConsumptionHeatPump" in measurement:
+            direct_consumtion_heatpump_sensor.set_state(float(measurement["directConsumptionHeatPump"]))
         
        
         time.sleep(WAIT)
