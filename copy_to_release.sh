a004202b985530fb3464c45c3cb961e85f293397
Simon Helming <simon@helming.xyz>
2024-05-08 00:42:12 +0200

    fix: added remove Content of release

--- copy_to_release.sh
+++ copy_to_release.sh
@@ -4,5 +4,7 @@
 src_dir="GridboxConnectorAddon-dev"
 target_dir="GridboxConnectorAddon"
 
+# Remove contents of target directory
+rm -rf $target_dir/*
 # Copy all files from source to target directory, overwriting existing files
 cp -R $src_dir/* $target_dir/
