73df18a952085ccb6947d21037db5d42504ad5c3
Simon Helming <simon@helming.xyz>
2024-05-08 01:17:37 +0200

    fix: release script

--- copy_to_release.sh
+++ copy_to_release.sh
@@ -5,4 +5,4 @@ src_dir="GridboxConnectorAddon-dev"
 target_dir="GridboxConnectorAddon"
 
 # Copy all files from source to target directory, excluding config.yml and build.yml
-rsync -av --exclude='config.yml' --exclude='build.yml' $src_dir/ $target_dir/
+rsync -av --exclude='config.yaml' --exclude='build.yaml' $src_dir/ $target_dir/
