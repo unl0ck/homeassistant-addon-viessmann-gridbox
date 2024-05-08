e21291976410669d810695dedb929ceb6a3f16eb
unl0ck <sa_helming@icloud.com>
2024-05-08 07:27:43 +0000

    release: 1.9.10 revert to 1.5.0 need data

--- GridboxConnectorAddon/GridboxConnector/__main__.py
+++ GridboxConnectorAddon/GridboxConnector/__main__.py
@@ -4,17 +4,17 @@ import time
 from ha_mqtt_discoverable import Settings, DeviceInfo
 from ha_mqtt_discoverable.sensors import Sensor, SensorInfo
 from gridbox_connector import GridboxConnector
-from Battery import Battery
+
 
 if __name__ == '__main__':
     f = open('/build/cloudSettings.json')
-    # returns JSON object as 
+    # returns JSON object as
     # a dictionary
     data = json.load(f)
     f.close()
     print("Start Viessmann Connector")
     #print(f"====Version {data["version"]}====")
-    
+
     options_file = open('/data/options.json')
     options_json = json.load(options_file)
     WAIT = int(options_json["wait_time"])
@@ -28,8 +28,6 @@ if __name__ == '__main__':
     data["login"]["password"] = PASSWORD
     print(data["login"])
     one_time_print = True
-
-    battery_dict = {}
 
     mqtt_settings = Settings.MQTT(host=mqtt_server, username=mqtt_user, password=mqtt_pw)
 
@@ -50,7 +48,7 @@ if __name__ == '__main__':
 
     total_consumption_household_sensor_info = SensorInfo(name="Total Consumption", device_class="power", unique_id="total_consumption_household", device=device_info, unit_of_measurement="W")
     total_consumption_household_settings = Settings(mqtt=mqtt_settings, entity=total_consumption_household_sensor_info)
-    
+
     # Direct Consumption
     direct_consumption_household_sensor_info = SensorInfo(name="DirectConsumptionHousehold", device_class="power", unique_id="gridbox_direct_consumption_household", device=device_info, unit_of_measurement="W")
     direct_consumption_household_settings = Settings(mqtt=mqtt_settings, entity=direct_consumption_household_sensor_info)
@@ -60,19 +58,19 @@ if __name__ == '__main__':
 
     direct_consumption_rate_sensor_info = SensorInfo(name="DirectConsumptionRate", device_class="power_factor", unique_id="gridbox_direct_consumption_rate", device=device_info, unit_of_measurement="%")
     direct_consumption_rate_settings = Settings(mqtt=mqtt_settings, entity=direct_consumption_rate_sensor_info)
-    
-    
+
+
     # Self Consumption
     self_supply_sensor_info = SensorInfo(name="SelfSupply", device_class="power", unique_id="gridbox_self_supply", device=device_info, unit_of_measurement="W")
     self_supply_settings = Settings(mqtt=mqtt_settings, entity=self_supply_sensor_info)
-    
+
     self_consumption_rate_sensor_info = SensorInfo(name="SelfConsumptionRate", device_class="power_factor", unique_id="gridbox_self_consumption_rate", device=device_info, unit_of_measurement="%")
     self_consumption_rate_settings = Settings(mqtt=mqtt_settings, entity=self_consumption_rate_sensor_info)
-    
+
     self_sufficiency_rate_sensor_info = SensorInfo(name="SelfSufficiencyRate", device_class="power_factor", unique_id="gridbox_self_sufficiency_rate", device=device_info, unit_of_measurement="%")
     self_sufficiency_rate_settings = Settings(mqtt=mqtt_settings, entity=self_sufficiency_rate_sensor_info)
-    
-    
+
+
     # Battery Section
     battery_sensor_info_sum = SensorInfo(name="Battery Sum Level", device_class="battery", unique_id="gridbox_battery_sum", device=device_info, unit_of_measurement="%")
     battery_settings_sum = Settings(mqtt=mqtt_settings, entity=battery_sensor_info_sum)
@@ -88,8 +86,10 @@ if __name__ == '__main__':
     grid_sensor = Sensor(grid_settings)
     photovoltaic_sensor = Sensor(photovoltaic_settings)
 
-    # Battery sum
-    battery_sum = Battery(mqtt_settings, device_info, "sum", "")
+    # Battery
+    battery_sum_level = Sensor(battery_settings_sum)
+    battery_sum_capacity = Sensor(battery_settings_capacity_sum)
+    battery_sum_power = Sensor(battery_settings_power_sum)
 
     # Consumption
     consumption_household_sensor = Sensor(consumption_household_settings)
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     direct_consumption_household_sensor = Sensor(direct_consumption_household_settings)
     direct_consumtion_heatpump_sensor = Sensor(direct_consumption_heatpump_settings)
     direct_consumtion_rate_sensor = Sensor(direct_consumption_rate_settings)
-    
+
     self_supply_sensor = Sensor(self_supply_settings)
     self_consumtion_rate_sensor = Sensor(self_consumption_rate_settings)
 
@@ -124,7 +124,7 @@ if __name__ == '__main__':
         if "directConsumptionRate" in measurement:
             direct_consumtion_rate_sensor.set_state(float(measurement["directConsumptionRate"])*100)
 
-        
+
         if "selfSupply" in measurement:
             self_supply_sensor.set_state(float(measurement["selfSupply"]))
         if "selfConsumptionRate" in measurement:
@@ -132,24 +132,10 @@ if __name__ == '__main__':
         if "selfSufficiencyRate" in measurement:
             self_consumtion_rate_sensor.set_state(float(measurement["selfSufficiencyRate"])*100)
 
-            
+
         if "battery" in measurement:
-            appliance_id = measurement["battery"]["applianceID"]
-            state_of_charge = float(measurement["battery"]["stateOfCharge"])*100
-            capacity = float(measurement["battery"]["capacity"])
-            power = float(measurement["battery"]["power"])
-            remaining_charge = float(measurement["battery"]["remainingCharge"])
-            battery_sum.set_values(state_of_charge, capacity, power, remaining_charge)
-        if "batteries" in measurement:
-            for index, battery in enumerate(measurement["batteries"]):
-                appliance_id = battery["applianceID"]
-                if appliance_id not in battery_dict:
-                    battery_dict[appliance_id] = Battery(mqtt_settings, device_info, f"{index}", appliance_id)
-                battery = battery_dict[appliance_id]
-                state_of_charge = float(measurement["battery"]["stateOfCharge"])*100
-                capacity = float(measurement["battery"]["capacity"])
-                power = float(measurement["battery"]["power"])
-                remaining_charge = float(measurement["battery"]["remainingCharge"])
-                battery.set_values(state_of_charge, capacity, power, remaining_charge)
+            battery_sum_level.set_state(float(measurement["battery"]["stateOfCharge"])*100)
+            battery_sum_capacity.set_state(float(measurement["battery"]["capacity"]))
+            battery_sum_power.set_state(float(measurement["battery"]["power"]))
         # Wait until fetch new values in seconds
         time.sleep(WAIT)
