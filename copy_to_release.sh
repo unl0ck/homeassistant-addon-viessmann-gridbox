e70894bc7c26f18994dfe85c5809475761f8fea2
Simon Helming <simon@helming.xyz>
2024-05-09 23:44:25 +0200

    ci: modified release script added cloudSettings.json

--- copy_to_release.sh
+++ copy_to_release.sh
@@ -6,3 +6,9 @@ target_dir="GridboxConnectorAddon"
 
 # Copy all files from source to target directory, excluding config.yml and build.yml
 rsync -av --exclude='config.yaml' --exclude='build.yaml' --exclude='__pycache__' --exclude='.pytest_cache' $src_dir/ $target_dir/
+# Read the current version from .bumpversion-dev.toml
+current_version=$(jq -r '.version' GridboxConnectorAddon/rootfs/share/cloudSettings.json)
+
+# Update the version in the config.yaml file
+sed -i '' "s/version: \".*\"/version: \"$current_version\"/" GridboxConnectorAddon/config.yaml
+
