6ae645d4dd6dc84129a059c64a7211d2d3f5d7b1
unl0ck <sa_helming@icloud.com>
2024-05-14 07:16:27 +0000

    ci: fixed copy_to_release script replace version string

--- copy_to_release.sh
+++ copy_to_release.sh
@@ -15,4 +15,4 @@ echo "New version: $new_version"
 
 # Update the version in the config.yaml file
 
-sed -i "s/version: $current_version/version: $new_version/g" $yaml_datei
+sed -i "s/version: \"$current_version\"/version: \"$new_version\"/g" $yaml_datei
