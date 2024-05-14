98d8bdb2192ab145e62732394091eee48a350dbc
unl0ck <sa_helming@icloud.com>
2024-05-14 09:55:54 +0000

    feat: working on s6-overlay

---
+++ GridboxConnectorAddon-experimental/rootfs/etc/s6-overlay/s6-rc.d/usr/bin/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bashio
+
+export MqttUser=$(bashio::config 'OverrideMqttUser')
+export MqttPw=$(bashio::config 'OverrideMqttPw')
+export MqttServer=$(bashio::config 'OverrideMqttServer')
+export MqttPort=$(bashio::config 'OverrideMqttPort')
+
+test "$MqttUser" = "null" && export MqttUser=$(bashio::services "mqtt" "username")
+test "$MqttPw" = "null" && export MqttPw=$(bashio::services "mqtt" "password")
+test "$MqttServer" = "null" && export MqttServer=$(bashio::services "mqtt" "host")
+test "$MqttPort" = "null" && export MqttPort=$(bashio::services "mqtt" "port")
+export USERNAME=$(bashio::config 'username')
+export PASSWORD=$(bashio::config 'password')
+ls -lash /data
+cat /data/options.json
+cd /build/
+ls -lash
+pwd
+ls -lash /etc/gridboxconnector
+python GridboxConnector
