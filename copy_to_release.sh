a9bca98a94cde5c49e1cf14d998edd77b4bd99fd
unl0ck <sa_helming@icloud.com>
2024-05-14 07:10:47 +0000

    ci: fixed release_stable shell script

--- copy_to_release.sh
+++ copy_to_release.sh
@@ -6,9 +6,13 @@ target_dir="GridboxConnectorAddon"
 
 # Copy all files from source to target directory, excluding config.yml and build.yml
 rsync -av --exclude='config.yaml' --exclude='build.yaml' --exclude='__pycache__' --exclude='.pytest_cache' $src_dir/ $target_dir/
-# Read the current version from .bumpversion-dev.toml
-current_version=$(jq -r '.version' GridboxConnectorAddon/rootfs/share/cloudSettings.json)
+
+yaml_datei="GridboxConnectorAddon/config.yaml"
+current_version=$(yq e '.version' $yaml_datei)
+echo "Current version: $current_version"
+new_version=$(jq -r '.version' GridboxConnectorAddon/rootfs/share/cloudSettings.json)
+echo "New version: $new_version"
 
 # Update the version in the config.yaml file
-sed -i '' "s/version: \".*\"/version: \"$current_version\"/" GridboxConnectorAddon/config.yaml
 
+sed -i "s/version: $current_version/version: $new_version/g" $yaml_datei
