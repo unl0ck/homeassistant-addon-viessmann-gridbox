af8b58408feddadc44746f881cddddc475ea9a05
unl0ck <sa_helming@icloud.com>
2024-11-12 08:39:07 +0000

    #42 fixed

--- GridboxConnectorAddon-dev/GridboxConnector/utils.py
+++ GridboxConnectorAddon-dev/GridboxConnector/utils.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import ast
+import os
 class SensitiveDataFilter(logging.Filter):
     def filter(self, record):
         message = record.getMessage()
@@ -19,8 +20,18 @@ class SensitiveDataFilter(logging.Filter):
                 literal_msg['client_id'] = '***'
             # Das modifizierte Dictionary zurück in einen String konvertieren
             record.msg = json.dumps(literal_msg)
-        except json.JSONDecodeError:
+        except Exception as e:
             # Wenn die Nachricht kein JSON ist, nichts tun
-            logging.error('Could not parse message as JSON')
+            logging.error(f"Error filtering sensitive data: {e}")
             pass
         return True
+
+def get_bool_env(var, default=False):
+    value = os.getenv(var, default)
+    if isinstance(value, str):
+        value = value.lower()
+        if value in ["1", "true"]:
+            return True
+        if value in ["0", "false"]:
+            return False
+    return bool(value)
