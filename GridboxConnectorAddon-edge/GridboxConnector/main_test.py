583b72dca2ee24373e71d5b2158269b5bde74d80
unl0ck <sa_helming@icloud.com>
2025-01-30 20:04:44 +0000

    👌 IMPROVE: added historical test

--- GridboxConnectorAddon-edge/GridboxConnector/main_test.py
+++ GridboxConnectorAddon-edge/GridboxConnector/main_test.py
@@ -6,7 +6,7 @@ from ha_mqtt_discoverable import Settings
 from ha_viessmann_gridbox_connector import HAViessmannGridboxConnector
 from paho.mqtt.client import MQTT_ERR_SUCCESS
 from ha_mqtt_discoverable.sensors import Sensor
-os.environ['LOG_LEVEL'] = 'ERROR'
+
 class TestGridboxConnectorMethods(unittest.TestCase):
 
     @patch('paho.mqtt.client.Client')
@@ -116,9 +116,49 @@ class TestGridboxConnectorMethods(unittest.TestCase):
         mqtt_settings = Settings.MQTT(host=mqtt_server, username=mqtt_user, password=mqtt_pw)
         viessmann_gridbox_connector = HAViessmannGridboxConnector(mqtt_settings)
 
-        with patch.object(viessmann_gridbox_connector.heater_sensor, 'set_states') as mock_heater_sensor:
-            viessmann_gridbox_connector.update_sensors(result[0], last_reset=None)
-            mock_heater_sensor.assert_called_once_with(3676,70.9)
+
+
+    @patch('paho.mqtt.client.Client')
+    @patch.object(GridboxConnector, 'init_auth', return_value=None)
+    @patch.object(GridboxConnector, '__init__', return_value=None)
+    @patch.object(GridboxConnector, 'retrieve_historical_data')
+    def test_main_heater(self, mock_retrieve_historical_data, mock_init, mock_init_auth,mock_mqtt_client):
+        # Load mock data from JSON file
+        with open('tests/mock_data/mock_data_historical_with_heater_and_battery.json') as f:
+            mock_data = [json.load(f)]
+        mock_mqtt_client.return_value.connect.return_value = MQTT_ERR_SUCCESS
+        mock_retrieve_historical_data.return_value = mock_data
+
+        from datetime import datetime, timezone, timedelta
+        mock_now = datetime(2023, 1, 1, 12, 0, 0, tzinfo=timezone(timedelta(hours=1)))
+        mock_now = mock_now.replace(hour=0, minute=0, second=0, microsecond=0)
+        today = mock_now.isoformat()
+        tomorrow = (mock_now + timedelta(days=1)).isoformat()
+        midnight_today = mock_now.replace(hour=0, minute=0, second=0, microsecond=0)
+        with patch('datetime.datetime') as mock_datetime:
+            mock_datetime.now.return_value = mock_now
+            mock_datetime.side_effect = lambda *args, **kw: datetime(*args, **kw)
+        # Create an instance of the class
+        gridbox_connector = GridboxConnector(None)
+
+        # Call the function
+        result = gridbox_connector.retrieve_historical_data(today, tomorrow)
+
+        # Assert the function was called once
+        mock_retrieve_historical_data.assert_called_once()
+
+        # Assert the function returned the mock value
+        self.assertEqual(result, mock_data)
+        mqtt_server = "mqtt_server"
+        mqtt_user = "mqtt_user"
+        mqtt_pw = "mqtt_pw"
+        mqtt_settings = Settings.MQTT(host=mqtt_server, username=mqtt_user, password=mqtt_pw)
+        viessmann_gridbox_historical_device = HAViessmannGridboxConnector(mqtt_settings=mqtt_settings, device_name="Viessmann Gridbox Historical", device_identifiers="viessmann_gridbox_historical", prefix="historical", unit_of_power="Wh", device_class_of_power="energy", state_class="total")
+
+        with patch.object(viessmann_gridbox_historical_device.battery_sum, 'set_states') as mock_battery_sum_sensor:
+            viessmann_gridbox_historical_device.update_sensors(result[0], last_reset=midnight_today.isoformat())
+            mock_battery_sum_sensor.assert_called_once_with(3.3195323553890965, 6354.166666666667, 0.0, 0.0, 548.55, 3612.91, '2023-01-01T00:00:00+01:00')
+
 
     def test_logger(self):
         import os
