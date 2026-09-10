15d7aec21b1b8f4a62a86d4a6c379296bb85b900
unl0ck <simon@helming.xyz>
2026-09-10 12:50:41 +0200

    fix: update gridX authentication dependency

--- GridboxConnectorAddon-edge/GridboxConnector/tests/main_test.py
+++ GridboxConnectorAddon-edge/GridboxConnector/tests/main_test.py
@@ -93,5 +93,5 @@ def test_logger():
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
     logger.addFilter(SensitiveDataFilter())
-    login_message = "{'grant_type': 'http://auth0.com/oauth/grant-type/password-realm', 'username': 'username@username', 'password': 'UltraSecret', 'audience': 'my.gridx', 'client_id': 'oZpr934Ikn8OZOHTJEcrgXkjio0I0Q7b', 'scope': 'email openid', 'realm': 'viessmann-authentication-db', 'client_secret': ''}"
+    login_message = "{'grant_type': 'http://auth0.com/oauth/grant-type/password-realm', 'username': 'username@username', 'password': 'UltraSecret', 'audience': 'https://api.gridx.de', 'client_id': 'oZpr934Ikn8OZOHTJEcrgXkjio0I0Q7b', 'scope': 'email openid', 'realm': 'viessmann-authentication-db', 'client_secret': ''}"
     logger.info(login_message)
